86262dddfcd934d3b47b3cb41e1f472be335654a
codearmando <armandowork36@gmail.com>
2022-10-13 13:07:30 -0400

    union de nav,barra lateral y vista hospitalizacion

--- layout/nav_superior.php
+++ layout/nav_superior.php
@@ -8,67 +8,70 @@
     <title>Document</title>
     <link rel="stylesheet" href="../asset/css/nav_superior.css">
     <link rel="stylesheet" href="../asset/fonts/style.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../asset/libs/bootstrap5/css/bootstrap.min.css">
 </head>
 
 <body>
-    <nav>
-        <div class="cont-nav">
-            <div class="menu_icon">
-                <div class="icono">
-                    <i id="icono_menu" for="" class="icon-menu" onclick="menu()"></i>
+    <div class="barra_superio">
+        <nav>
+            <div class="cont-nav">
+                <div class="menu_icon">
+                    <div class="icono">
+                        <i id="icono_menu" for="" class="icon-menu" onclick="menu()"></i>
 
+                    </div>
+                    <div class="logo">
+                        <img src="asset/img/IVSS_blanco.png" width="50px" alt="">
+                    </div>
                 </div>
-                <div class="logo">
-                    <img src="asset/img/IVSS_blanco.png" width="50px" alt="">
+
+                <div class="user">
+                    <ul>
+                        <li>
+                            <a href="#">Nombre de Usuario</a>
+                            <ul>
+                                <li>
+                                    <a href="#">Clouse Session</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </div>
+        </nav>
+        <!-- barra lateral -->
 
-            <div class="user">
+        <div class="cont-sidebar">
+            <div class="sidebar" id="sidebar">
+                <div class="encabezado_sidebar">
+                    <div class="clouse_sidebar">
+                        <span class="icon-cross" id="clouse_bar" onclick="clouse()"></span>
+                    </div>
+                    <center>
+                        <div class="logo">
+                            <img src="asset/img/IVSS_blanco.png" width="100px" alt="">
+                        </div>
+                        <div class="user_sidebar">
+                            <h3>NOMBRE DEL USUARIO</h3>
+                        </div>
+                    </center>
+
+                </div>
                 <ul>
+                    <li class="activ_enlaces"><a href="#"><i class="icon-home "> Inicio</i> </a></li>
+                    <li><a href="#"><i class="icon-user"> Registrar Paciente</i></a></li>
                     <li>
-                        <a href="#">Nombre de Usuario</a>
-                        <ul>
-                            <li>
-                                <a href="#">Clouse Session</a>
-                            </li>
+                        <a href="#" id="sub_menu"><i class="icon-aid-kit"> Centro Hospitalarios</i>
+                            <span class="icon-circle-down first"></span>
+                        </a>
+                        <ul id="sub_menu_ul_uno">
+                            <li><a href="#">sabado Luciani</a></li>
+                            <li><a href="#">Domingo Luciani</a></li>
+                            <li><a href="#">Lunes Luciani</a></li>
                         </ul>
                     </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- barra lateral -->
-
-    <div class="cont-sidebar">
-        <div class="sidebar" id="sidebar">
-            <div class="encabezado_sidebar">
-                <div class="clouse_sidebar">
-                    <span class="icon-cross" id="clouse_bar" onclick="clouse()"></span>
-                </div>
-                <center>
-                    <div class="logo">
-                        <img src="asset/img/IVSS_blanco.png" width="100px" alt="">
-                    </div>
-                    <div class="user_sidebar">
-                        <h3>NOMBRE DEL USUARIO</h3>
-                    </div>
-                </center>
-
-            </div>
-            <ul>
-                <li class="activ_enlaces"><a href="#"><i class="icon-home "> Inicio</i> </a></li>
-                <li><a href="#"><i class="icon-user"> Registrar Paciente</i></a></li>
-                <li>
-                    <a href="#" id="sub_menu"><i class="icon-aid-kit"> Centro Hospitalarios</i>
-                        <span class="icon-circle-down first"></span>
-                    </a>
-                    <ul id="sub_menu_ul_uno">
-                        <li><a href="#">sabado Luciani</a></li>
-                        <li><a href="#">Domingo Luciani</a></li>
-                        <li><a href="#">Lunes Luciani</a></li>
-                    </ul>
-                </li>
-                <!-- <li>
+                    <!-- <li>
                     <a href="#" id="sub_menu_dos"><i class="icon-home"> Centro de amor</i>
                         <span class="icon-circle-down second"></span>
                     </a>
@@ -78,9 +81,10 @@
                         <li><a href="#">Lunes2 Luciani2</a></li>
                     </ul>
                 </li> -->
-                <li><a href="#"><i class="icon-key"> Contraseña</i></a></li>
-                <li><a href="#"><i class="icon-share"> Salir</i></a></li>
-            </ul>
+                    <li><a href="#"><i class="icon-key"> Contraseña</i></a></li>
+                    <li><a href="#"><i class="icon-share"> Salir</i></a></li>
+                </ul>
+            </div>
         </div>
     </div>
 
--- layout/vista_hospitalizacion.php
+++ layout/vista_hospitalizacion.php
@@ -1,92 +1,152 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hospitalaización</title>
     <!-- estilos -->
-    <link rel="stylesheet" href="../asset/css/nav_superior.css">
+    <link rel="stylesheet" href="../asset/css/vista_hospitalizacion.css">
     <link rel="stylesheet" href="../asset/fonts/style.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../asset/libs/bootstrap5/css/bootstrap.min.css">
+    <!-- datatable -->
+    <link rel="stylesheet" href="../asset/libs/dataTables/datatables.min.css">
 </head>
+
 <body>
-    <div class="mini_encabezado">
-        <div class="nombre_hosp">
-            <span>HOSPITAL DOMINGO LUCIANI </span>
-        </div>
-        <div class="piso_hosp">
-            <span>PISO 1</span>
-        </div>
-        <div class="area_trabajo">
-            <span>GINECO-OBSTRETICIA</span>
-        </div>
-        <div class="date_time">
-            <span>26/09/2022</span>
-            <span>12:13 PM</span>
-        </div>
-    </div>
-    <section class="modulo_hospitalizacion">
-
-        <div class="tit">
-            <h2>HOSPITALIZACIÓN - GINECO - OBSTRETICIA(MODULO A)</h2>
-        </div>
-
-        <div class="cont_general">
-            <!-- modal -->
-            <button>NUEVO</button>
-
-            <div class="camas">
-                <div class="disp">
-                    <span>400</span>
-                </div>
-                <div class="ocup">
-                    <span>400</span>
-                </div>
+    <!-- php nav -->
+    <?php require 'nav_superior.php' ?>
+    <div class="cont_hospitalizacion">
+        <div class="mini_encabezado">
+            <div class="nombre_hosp">
+                <span>HOSPITAL DOMINGO LUCIANI </span>
+            </div>
+            <div class="piso_hosp">
+                <span>PISO 1</span>
+            </div>
+            <div class="area_trabajo">
+                <span>GINECO-OBSTRETICIA</span>
+            </div>
+            <div class="date_time">
+                <span>26/09/2022</span>
+                <span>12:13 PM</span>
             </div>
         </div>
+        <section class="modulo_hospitalizacion">
 
-        <table id="list_hospitalizados">
-            <thead>
-                <tr>
-                    <th>Nombre del Paciente</th>
-                    <th>Habitación</th>
-                    <th>Cama</th>
-                    <th>Fecha Ingreso</th>
-                    <th>Estatus</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Jaun Castillo</td>
-                    <td>HAB123</td>
-                    <td>0CAM</td>
-                    <td>23/09/2001</td>
-                    <td>vivo</td>
-                </tr>
-                <tr>
-                    <td>jesus Colina</td>
-                    <td>HAB456</td>
-                    <td>1CAM</td>
-                    <td>13/12/2023</td>
-                    <td>vivo</td>
-                </tr>
-                <tr>
-                    <td>Rosa Guadalupe</td>
-                    <td>HAB789</td>
-                    <td>2CAM</td>
-                    <td>13/02/2013</td>
-                    <td>vivo</td>
-                </tr>
-                <tr>
-                    <td>yulimar Cedeño/td>
-                    <td>HAB101</td>
-                    <td>3CAM</td>
-                    <td>31/07/199</td>
-                    <td>vivo</td>
-                </tr>
-                
-            </tbody>
-        </table>
-    </section>
+            <div class="tit">
+                <h2>HOSPITALIZACIÓN - GINECO - OBSTRETICIA(MODULO A)</h2>
+            </div>
+
+            <div class="cont_general">
+                <!-- modal -->
+                <button>NUEVO</button>
+
+                <div class="camas">
+                    <div class="disp cdo">
+                        <h6>Camas Disponibles</h6>
+                        <strong><span class="num_disp" style="color:#5bdeff">400</span></strong>
+                    </div>
+                    <div class="ocup cdo">
+                        <h6>Camas Ocupadas</h6>
+                        <strong><span class="num_ocu" style="color:#eeff00">100</span></strong>
+                    </div>
+                </div>
+            </div>
+
+            <table id="list_hospitalizados" width="100%" height= "100vh">
+                <thead>
+                    <tr>
+                        <th>Nombre del Paciente</th>
+                        <th>Habitación</th>
+                        <th>Cama</th>
+                        <th>Fecha Ingreso</th>
+                        <th>Estatus</th>
+                        <th>Modificar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Jaun Castillo</td>
+                        <td>HAB123</td>
+                        <td>0CAM</td>
+                        <td>23/09/2001</td>
+                        <td>vivo</td>
+                        <td>modificar</td>
+                        <td>Eliminar</td>
+                    </tr>
+                    <tr>
+                        <td>jesus Colina</td>
+                        <td>HAB456</td>
+                        <td>1CAM</td>
+                        <td>13/12/2023</td>
+                        <td>vivo</td>
+                        <td>modificar</td>
+                        <td>Eliminar</td>
+                    </tr>
+                    <tr>
+                        <td>Rosa Guadalupe</td>
+                        <td>HAB789</td>
+                        <td>2CAM</td>
+                        <td>13/02/2013</td>
+                        <td>vivo</td>
+                        <td>modificar</td>
+                        <td>Eliminar</td>
+                    </tr>
+                    <tr>
+                        <td>yulimar Cedeño/td>
+                        <td>HAB101</td>
+                        <td>3CAM</td>
+                        <td>31/07/199</td>
+                        <td>vivo</td>
+                        <td>modificar</td>
+                        <td>Eliminar</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </section>
+    </div>
+
+    <!-- jquery -->
+    <script src="..//asset/libs/jquery/jquery-3.3.1.min.js"></script>
+    <!-- bootstrap -->
+    <script src="..//asset/libs/bootstrap5/js/bootstrap.min.js"></script>
+    <!-- datatable -->
+    <script src="..//asset/libs/dataTables/datatables.min.js"></script>
+    <!-- llamando tabla con datatable  -->
+    <script>
+        $(document).ready(function() {
+            $('#list_hospitalizados').DataTable();
+        });
+        // datatable en español
+        var table = $('#list_hospitalizados').DataTable({
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            },
+
+        });
+    </script>
 </body>
+
 </html>

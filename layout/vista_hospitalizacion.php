397db149cbe96ab6c556ac052247f4aa39cbffee
codearmando <armandowork36@gmail.com>
2022-10-13 10:50:55 -0400

    nuevo archivo de css vista hospitalizacion y nuevo nombre a vista hospitalizacion php

---
+++ layout/vista_hospitalizacion.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospitalaización</title>
+    <!-- estilos -->
+    <link rel="stylesheet" href="../asset/css/nav_superior.css">
+    <link rel="stylesheet" href="../asset/fonts/style.css">
+</head>
+<body>
+    <div class="mini_encabezado">
+        <div class="nombre_hosp">
+            <span>HOSPITAL DOMINGO LUCIANI </span>
+        </div>
+        <div class="piso_hosp">
+            <span>PISO 1</span>
+        </div>
+        <div class="area_trabajo">
+            <span>GINECO-OBSTRETICIA</span>
+        </div>
+        <div class="date_time">
+            <span>26/09/2022</span>
+            <span>12:13 PM</span>
+        </div>
+    </div>
+    <section class="modulo_hospitalizacion">
+
+        <div class="tit">
+            <h2>HOSPITALIZACIÓN - GINECO - OBSTRETICIA(MODULO A)</h2>
+        </div>
+
+        <div class="cont_general">
+            <!-- modal -->
+            <button>NUEVO</button>
+
+            <div class="camas">
+                <div class="disp">
+                    <span>400</span>
+                </div>
+                <div class="ocup">
+                    <span>400</span>
+                </div>
+            </div>
+        </div>
+
+        <table id="list_hospitalizados">
+            <thead>
+                <tr>
+                    <th>Nombre del Paciente</th>
+                    <th>Habitación</th>
+                    <th>Cama</th>
+                    <th>Fecha Ingreso</th>
+                    <th>Estatus</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Jaun Castillo</td>
+                    <td>HAB123</td>
+                    <td>0CAM</td>
+                    <td>23/09/2001</td>
+                    <td>vivo</td>
+                </tr>
+                <tr>
+                    <td>jesus Colina</td>
+                    <td>HAB456</td>
+                    <td>1CAM</td>
+                    <td>13/12/2023</td>
+                    <td>vivo</td>
+                </tr>
+                <tr>
+                    <td>Rosa Guadalupe</td>
+                    <td>HAB789</td>
+                    <td>2CAM</td>
+                    <td>13/02/2013</td>
+                    <td>vivo</td>
+                </tr>
+                <tr>
+                    <td>yulimar Cedeño/td>
+                    <td>HAB101</td>
+                    <td>3CAM</td>
+                    <td>31/07/199</td>
+                    <td>vivo</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </section>
+</body>
+</html>
